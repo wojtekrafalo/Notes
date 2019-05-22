c442bce19338fbfda11cf8cbad08f17870048ba8
wojtekrafalo <wojtekrafalo@wp.pl>
2019-05-22 19:57:05 +0200

    A Listener of EditText-fragment almost complete.

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,32 +1,30 @@
 package com.example.notes
 
-import android.app.Activity
 import android.graphics.Color
 import android.graphics.Typeface
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
-import android.text.SpannableString
 import android.text.SpannableStringBuilder
-import android.text.style.CharacterStyle
 import android.text.style.ForegroundColorSpan
-import android.text.style.TypefaceSpan
 import android.text.style.UnderlineSpan
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.Toast
-import com.example.notes.R
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.my_fragment.*
+import kotlinx.android.synthetic.main.fragment_edit_text.*
 import top.defaults.colorpicker.ColorPickerPopup
-import java.lang.NullPointerException
-import java.util.*
 
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, EditTextFragment.OnFragmentInteractionListener {
     private var mySpannable : SpannableStringBuilder = SpannableStringBuilder()
+//    private lateinit var fragEditText: EditText
+    private var eText : Int = 0
+    private lateinit var edit : android.support.v7.widget.AppCompatEditText
+    private var numberOfFragments:Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,48 +39,47 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             spinner.adapter = adapter
         }
         spinner.onItemSelectedListener = this
+
+//        println(editText::class.java.toString())
+
+//        fragEditText.setOnTouchListener { v, clickEvent ->
+//            eText = v.id
+//            v.setBackgroundColor(Color.RED)
+//            edit = v as AppCompatEditText
+//
+//            println("KLIKNIETY_DEBILU  " + edit.text.toString())
+//            true
+//        }
+
+//        addEditTextFragment()
     }
 
     fun clickU (view: View) {
-//        Toast.makeText(this, editText.text[editText.selectionStart].toString(), Toast.LENGTH_SHORT).show()
-        mySpannable = SpannableStringBuilder(editText.text.toString())
+        mySpannable = SpannableStringBuilder(fragEditText.text.toString())
         mySpannable.setSpan(
             UnderlineSpan(),
-            editText.selectionStart, editText.selectionEnd,
+            fragEditText.selectionStart, fragEditText.selectionEnd,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-        editText.text = mySpannable
-
-//        var next:Int
-//        var i=0
-//        val size:Int=spannable.length
-//
-//        val spanList = spannable.getSpans(0, size, Object::class.java)
-//
-//        while(i < size-1) {
-////            next = spannable.nextSpanTransition(i, size, CharacterStyle::class.java)
-//            spannable.setSpan(spanList[i], spannable.getSpanStart(spanList[i]),spannable.getSpanEnd(spanList[i]), spannable.getSpanFlags(spanList[i]))
-//            i++
-//        }
+        fragEditText.text = mySpannable
     }
 
     fun clickB (view: View) {
-
-        mySpannable = SpannableStringBuilder(editText.text.toString())
+        mySpannable = SpannableStringBuilder(fragEditText.text.toString())
         mySpannable.setSpan(
             android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
-            editText.selectionStart, editText.selectionEnd,
+            fragEditText.selectionStart, fragEditText.selectionEnd,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        editText.text = mySpannable
+        fragEditText.text = mySpannable
     }
 
     fun clickI (view: View) {
-        mySpannable = SpannableStringBuilder(editText.text.toString())
+        mySpannable = SpannableStringBuilder(fragEditText.text.toString())
         mySpannable.setSpan(
             android.text.style.StyleSpan(android.graphics.Typeface.ITALIC),
-            editText.selectionStart, editText.selectionEnd,
+            fragEditText.selectionStart, fragEditText.selectionEnd,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        editText.text = mySpannable
+        fragEditText.text = mySpannable
     }
 
     fun clickC (view: View) {
@@ -90,22 +87,12 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         class ColorDialog : ColorPickerPopup.ColorPickerObserver() {
             override fun onColorPicked(color: Int) {
 
-                mySpannable = SpannableStringBuilder(editText.text.toString())
+                mySpannable = SpannableStringBuilder(fragEditText.text.toString())
                 mySpannable.setSpan(
                     ForegroundColorSpan(color),
-                    editText.selectionStart, editText.selectionEnd,
+                    fragEditText.selectionStart, fragEditText.selectionEnd,
                     Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                editText.text = mySpannable
-
-//                editText.text.toString().forEach { c ->
-//                    var sbb = SimpleSpanBuilder( c.toString(), ForegroundColorSpan(color) )
-//                    sbb += SimpleSpanBuilder.Span()
-//                }
-//                var sbb = SimpleSpanBuilder(editText.text.toString(), ForegroundColorSpan(color))
-//                sbb += SimpleSpanBuilder.Span(
-//                )
-//                editText.text = sbb.build()
-
+                fragEditText.text = mySpannable
             }
             fun onColor(color: Int, fromUser: Boolean) {}
         }
@@ -124,17 +111,33 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     }
 
 
+    fun clickedEdit (view: View) {
+        mySpannable = SpannableStringBuilder(fragEditText.text.toString())
+        mySpannable.setSpan(
+            android.text.style.StyleSpan(android.graphics.Typeface.ITALIC),
+            fragEditText.selectionStart, fragEditText.selectionEnd,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        fragEditText.text = mySpannable
+    }
+
+
+    private fun addEditTextFragment (){
+        val frag:EditTextFragment = EditTextFragment.newInstance(numberOfFragments.toString(),"TRASH")
+//        frag.onAttach(this)
+        supportFragmentManager.beginTransaction().add(numberOfFragments, frag).commit()
+        frag.setListener(this)
+        numberOfFragments++
+    }
+
 
 
     override fun onNothingSelected(parent: AdapterView<*>?) {}
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//            val spannable = SpannableStringBuilder(editText.text.toString())
-        editText.typeface = Typeface.create(parent!!.getItemAtPosition(position).toString(), Typeface.NORMAL)
-//            spannable.setSpan(
-//                TypefaceSpan(Typeface.createFromAsset(assets, "fonts/font_family_medium")),
-//                editText.selectionStart, editText.selectionEnd,
-//                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//            )
-//            editText.text = spannable
+        fragEditText.typeface = Typeface.create(parent!!.getItemAtPosition(position).toString(), Typeface.NORMAL)
+    }
+
+    override fun onFragmentInteractionEdit(editText:EditText) {
+//        this.fragEditText = editText
+        println("CAPTURED_MOTHERFUCKER " + editText.text.toString())
     }
 }
