d9bd185c71bd9cf34e3041c30b4cce8b5e82a807
wojtekrafalo <wojtekrafalo@wp.pl>
2019-05-26 23:20:17 +0200

    Small changes of a layout.

--- app/src/main/java/com/example/notes/MySQLiteHelper.kt
+++ app/src/main/java/com/example/notes/MySQLiteHelper.kt
@@ -20,8 +20,6 @@ class MySQLiteHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,
         onCreate(db)
     }
 
-
-    //TODO ten obiekt jest do poprawy jeśli będziesz robił bazę danych. Zmień wartość DATABASE_CREATE tak, żeby tworzyło tabele z notatkami
     companion object {
         val TABLE_EDIT_TEXT = "edit_text"
         val COLUMN_ID = "_id"
--- app/src/main/java/com/example/notes/NoteTextActivity.kt
+++ app/src/main/java/com/example/notes/NoteTextActivity.kt
@@ -14,7 +14,7 @@ import java.lang.Exception
 
 class NoteTextActivity : AppCompatActivity(), EditTextFragment.OnFragmentInteractionListener {
     override fun onFragmentInteractionEdit(editText: EditText) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     private lateinit var database : NotesDatabase
