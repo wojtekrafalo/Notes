8affb0463e52933e97dea62a331aa6ed651bff3c
wojtekrafalo <wojtekrafalo@wp.pl>
2019-05-26 14:31:35 +0200

    Created methods to store a data from Text Fields to a database. Also created first database classes.

--- app/src/main/java/com/example/notes/EditTextEntity.kt
+++ app/src/main/java/com/example/notes/EditTextEntity.kt
@@ -3,7 +3,6 @@ package com.example.notes
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
-import android.text.Html
 
 @Entity(tableName = "edit_text")
 data class EditTextEntity (
